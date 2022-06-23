fafbf2d86591f44ae47f2bb6869162594e7e99d5
aerou <houhin0809@gmail.com>
2022-06-23 10:50:03 +0800

    update

--- skiplist.h
+++ skiplist.h
@@ -3,9 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <mutex>
+#include <cstring>
 
 #define STORE_DIR "./store/dumpfile"
 
+//class template to implement node
 template<typename K, typename V>
 class Node {
 public:
@@ -34,5 +36,34 @@ template<typename K, typename V>
 K Node<K, V>::get_key() const
 {
 	return this->key;
+}
+
+template<typename K, typename V>
+V Node<K, V>::get_val() const
+{
+	return this->val;
+}
+
+template<typename K, typename V>
+void Node<K, V>::set_val(V cur_val)
+{
+	this->val = cur_val;
+}
+
+template<typename K, typename V>
+Node<K, V>::Node(const K k, const V v, int level)
+{
+	this->key = k;
+	this->val = v;
+	this->node_level = level;
+
+	this->next_node = new Node<K, V> *[level + 1];
+	memset(next_node, 0, sizeof(Node<K, V>*) * (level + 1));
+}
+
+template<typename K, typename V>
+Node<K, V>::~Node()
+{
+	delete []next_node;
 }
 
